3905bb287e531835fb121c68ceb3bb4ad0697a9b
phendev <stephen.bewers@gmail.com>
2022-03-11 16:19:23 +0000

    Update font import to fix broken font, and fix modal exit button position

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -18,11 +18,7 @@ import PokemonTraining from "../Pokemon/PokemonTraining";
 import PokemonTypeEffectiveness from "../Pokemon/PokemonTypeEffectiveness";
 import PokemonOtherForms from "../Pokemon/PokemonOtherForms";
 import PokemonEvolution from "../Pokemon/PokemonEvolution";
-import {
-  errorHandler,
-  cancelPromise,
-  getFormPromises,
-} from "../../helpers.js";
+import { errorHandler, cancelPromise, getFormPromises } from "../../helpers.js";
 import ModalRibbon from "./ModalRibbon";
 
 // Array that will store promises to return the current variant forms. Promises will be cancelled on unmount.
@@ -36,7 +32,7 @@ class Modal extends Component {
     this.refreshModal = this.refreshModal.bind(this);
     this.state = {
       pokemon: this.props.pokemon,
-      formsOfCurrentVariantReceived: false
+      formsOfCurrentVariantReceived: false,
     };
   }
   static propTypes = {
@@ -122,10 +118,7 @@ class Modal extends Component {
   render() {
     const { showModal, hideModal, filterBtnClick } = this.props;
 
-    const {
-      pokemon,
-      formsOfCurrentVariantReceived,
-    } = this.state;
+    const { pokemon, formsOfCurrentVariantReceived } = this.state;
 
     // If the showModal state becomes false, hide the modal
     const visibleClassName = showModal ? "visible" : "hidden";
@@ -139,29 +132,76 @@ class Modal extends Component {
           ref={this.modalMainRef}
         >
           <ModalRibbon pokemon={pokemon} />
-          <ModalImagePanel pokemon={pokemon} key={`image-${pokemon.variant.id}`} />
+          <ModalImagePanel
+            pokemon={pokemon}
+            key={`image-${pokemon.variant.id}`}
+          />
           <div className="modal-info-panel" ref={this.infoPanelRef}>
-            <PokemonDescription pokemon={pokemon} key={`description-${pokemon.species.id}`} />
+            <PokemonDescription
+              pokemon={pokemon}
+              key={`description-${pokemon.species.id}`}
+            />
             <ModalRow id="modal-top-row">
-              <PokemonTypes pokemon={pokemon} key={`types-${pokemon.variant.id}`} filterBtnClick={filterBtnClick} />
-              <PokemonHabitat pokemon={pokemon} key={`habitat-${pokemon.species.id}`} />
-              <PokemonCatchRate pokemon={pokemon} key={`catch-rate-${pokemon.species.id}`} />
-              <PokemonHeight pokemon={pokemon} key={`height-${pokemon.variant.id}`} />
-              <PokemonWeight pokemon={pokemon} key={`weight-${pokemon.variant.id}`} />
-              <PokemonGender pokemon={pokemon} key={`gender-${pokemon.species.id}`} />
+              <PokemonTypes
+                pokemon={pokemon}
+                key={`types-${pokemon.variant.id}`}
+                filterBtnClick={filterBtnClick}
+              />
+              <PokemonHabitat
+                pokemon={pokemon}
+                key={`habitat-${pokemon.species.id}`}
+              />
+              <PokemonCatchRate
+                pokemon={pokemon}
+                key={`catch-rate-${pokemon.species.id}`}
+              />
+              <PokemonHeight
+                pokemon={pokemon}
+                key={`height-${pokemon.variant.id}`}
+              />
+              <PokemonWeight
+                pokemon={pokemon}
+                key={`weight-${pokemon.variant.id}`}
+              />
+              <PokemonGender
+                pokemon={pokemon}
+                key={`gender-${pokemon.species.id}`}
+              />
             </ModalRow>
             <ModalRow id="modal-centre-section">
               <ModalColumn>
-                <PokemonBaseStats pokemon={pokemon} key={`base-stats-${pokemon.variant.id}`} />
-                <PokemonAbilities pokemon={pokemon} key={`abilities-${pokemon.variant.id}`} />
-                <PokemonTraining pokemon={pokemon} key={`training-${pokemon.variant.id}`} />
+                <PokemonBaseStats
+                  pokemon={pokemon}
+                  key={`base-stats-${pokemon.variant.id}`}
+                />
+                <PokemonAbilities
+                  pokemon={pokemon}
+                  key={`abilities-${pokemon.variant.id}`}
+                />
+                <PokemonTraining
+                  pokemon={pokemon}
+                  key={`training-${pokemon.variant.id}`}
+                />
               </ModalColumn>
               <ModalColumn>
-                <PokemonTypeEffectiveness pokemon={pokemon} key={`type-effectiveness-${pokemon.variant.id}`} filterBtnClick={filterBtnClick} />
+                <PokemonTypeEffectiveness
+                  pokemon={pokemon}
+                  key={`type-effectiveness-${pokemon.variant.id}`}
+                  filterBtnClick={filterBtnClick}
+                />
               </ModalColumn>
             </ModalRow>
-            <PokemonEvolution pokemon={pokemon} clickHandler={this.refreshModal} key={`evolution-${pokemon.variant.id}`} />
-            <PokemonOtherForms pokemon={pokemon} formsOfCurrentVariantReceived={formsOfCurrentVariantReceived} refreshModal={this.refreshModal} key={`other-forms-${pokemon.variant.id}`} />
+            <PokemonEvolution
+              pokemon={pokemon}
+              clickHandler={this.refreshModal}
+              key={`evolution-${pokemon.variant.id}`}
+            />
+            <PokemonOtherForms
+              pokemon={pokemon}
+              formsOfCurrentVariantReceived={formsOfCurrentVariantReceived}
+              refreshModal={this.refreshModal}
+              key={`other-forms-${pokemon.variant.id}`}
+            />
           </div>
         </section>
       </div>
