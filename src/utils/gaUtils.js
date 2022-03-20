b1a2091a9b71efa951e4878f497ffd2a18e6c79f
phendev <stephen.bewers@gmail.com>
2022-03-20 20:56:18 +0000

    Send GA pageview after initialisation

--- src/utils/gaUtils.js
+++ src/utils/gaUtils.js
@@ -5,5 +5,6 @@ import ReactGA from "react-ga4";
 export const initGA = (trackingId) => {
   if (process.env.NODE_ENV === "production") {
     ReactGA.initialize(trackingId);
+    ReactGA.send("pageview");
   }
 };
