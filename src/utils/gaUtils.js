b9f8ae3616490662594005b1ac7a6f1c1fe7d77d
phendev <stephen.bewers@gmail.com>
2022-03-20 18:53:27 +0000

    Fix import for ReactGA

--- src/utils/gaUtils.js
+++ src/utils/gaUtils.js
@@ -1,6 +1,6 @@
 /* This file contains utility functions for Google Analytics */
 
-import * as ReactGA from "react-ga4";
+import ReactGA from "react-ga4";
 
 export const initGA = (trackingId) => {
   if (process.env.NODE_ENV === "production") {
