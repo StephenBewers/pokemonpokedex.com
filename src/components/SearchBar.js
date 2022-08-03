5179a7569466f7056d62f3f903a560258d489505
phendev <stephen.bewers@gmail.com>
2022-08-03 08:14:40 +0100

    Rewrite search bar to use React Hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,39 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchBar.scss";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-class SearchBar extends Component {
-  static propTypes = {
-    options: PropTypes.instanceOf(Array).isRequired,
-    updatePokemonCardList: PropTypes.func,
-    clearSearchBar: PropTypes.bool,
-    searchBarCleared: PropTypes.func,
+const SearchBar = ({
+  options,
+  updatePokemonCardList,
+  clearSearchBar,
+  searchBarCleared,
+}) => {
+  const [activeOption, setActiveOption] = useState(0);
+  const [filteredOptions, setFilteredOptions] = useState([]);
+  const [showOptions, setShowOptions] = useState(false);
+  const [defaultView, setDefaultView] = useState(true);
+  const [userInput, setUserInput] = useState("");
+
+  const resetState = () => {
+    setActiveOption(0);
+    setFilteredOptions([]);
+    setShowOptions(false);
+    setDefaultView(true);
+    setUserInput("");
   };
 
-  state = {
-    activeOption: 0,
-    filteredOptions: [],
-    showOptions: false,
-    defaultView: true,
-    userInput: "",
-  };
-
-  componentDidUpdate = () => {
-    if (this.props.clearSearchBar) {
-      this.setState(
-        {
-          activeOption: 0,
-          filteredOptions: [],
-          showOptions: false,
-          userInput: "",
-        },
-        this.props.searchBarCleared()
-      );
+  useEffect(() => {
+    if (clearSearchBar) {
+      setActiveOption(0);
+      setFilteredOptions([]);
+      setShowOptions(false);
+      setUserInput("");
+      searchBarCleared();
     }
-  };
+  }, [clearSearchBar, searchBarCleared]);
 
-  filterOptions = (options, userInput) => {
+  const filterOptions = (options, userInput) => {
     // Get the pokemon that begin with the user input string
     const optionsStartingWithUserInput = options.filter((option) =>
       option.toLowerCase().startsWith(userInput.toLowerCase())
@@ -54,66 +53,51 @@ class SearchBar extends Component {
   };
 
   // Resets the view of the whole app to the default
-  resetView = () => {
-    if (!this.state.defaultView) {
-      this.props.updatePokemonCardList();
-      this.props.searchBarCleared();
+  const resetView = () => {
+    if (!defaultView) {
+      updatePokemonCardList();
+      searchBarCleared();
     }
-    this.setState({
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      defaultView: true,
-      userInput: "",
-    });
+    resetState();
   };
 
   // When the text in the search bar changes, filter the list of pokemon and display the suggestions
-  onChange = (event) => {
-    const { options } = this.props;
-    const userInput = event.currentTarget.value;
-    if (userInput.length) {
-      const filteredOptions = this.filterOptions(options, userInput);
-      this.setState({
-        activeOption: 0,
-        filteredOptions,
-        showOptions: true,
-        userInput,
-      });
+  const onChange = (event) => {
+    const userInputText = event.currentTarget.value;
+    if (userInputText.length) {
+      const filteredOptionsArray = filterOptions(options, userInputText);
+      setActiveOption(0);
+      setFilteredOptions(filteredOptionsArray);
+      setShowOptions(true);
+      setUserInput(userInputText);
     }
     // If user input has been cleared, reset the view
     else {
-      this.resetView();
+      resetView();
     }
   };
 
   // When the user clicks one of the autocomplete suggestions, populate the search box with
   // the clicked pokemon name, load that pokemon card, and stop displaying suggestions
-  onClick = (event) => {
-    this.setState({
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      defaultView: false,
-      userInput: event.target.outerText,
-    });
-    this.props.updatePokemonCardList([
-      { name: event.target.outerText.toLowerCase() },
-    ]);
+  const onClick = (event) => {
+    setActiveOption(0);
+    setFilteredOptions([]);
+    setShowOptions(false);
+    setDefaultView(false);
+    setUserInput(event.target.outerText);
+
+    updatePokemonCardList([{ name: event.target.outerText.toLowerCase() }]);
   };
 
   // On clicking the submit button, search for the currently selected pokemon in the list
-  onSubmit = (event) => {
-    const { activeOption, filteredOptions } = this.state;
+  const onSubmit = (event) => {
+    setActiveOption(0);
+    setFilteredOptions([]);
+    setShowOptions(false);
+    setDefaultView(false);
+    setUserInput(filteredOptions[activeOption]);
 
-    this.setState({
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      defaultView: false,
-      userInput: filteredOptions[activeOption],
-    });
-    this.props.updatePokemonCardList([
+    updatePokemonCardList([
       {
         name: filteredOptions[activeOption].toLowerCase(),
       },
@@ -121,12 +105,10 @@ class SearchBar extends Component {
   };
 
   // Handle key events for autocomplete suggestion list
-  onKeyDown = (event) => {
-    const { activeOption, filteredOptions } = this.state;
-
+  const onKeyDown = (event) => {
     // Return key provides the same action as the submit event
     if (event.keyCode === 13 && filteredOptions.length) {
-      this.onSubmit(event);
+      onSubmit(event);
     }
 
     // Up arrow selects the suggestion above the currently selected option if not already at the top
@@ -134,7 +116,7 @@ class SearchBar extends Component {
       if (activeOption === 0) {
         return;
       }
-      this.setState({ activeOption: activeOption - 1 });
+      setActiveOption((activeOption) => activeOption - 1);
     }
 
     // Down arrow selects the suggestion below the currently selected option if not already at the bottom
@@ -142,149 +124,139 @@ class SearchBar extends Component {
       if (activeOption - 1 === filteredOptions.length) {
         return;
       }
-      this.setState({ activeOption: activeOption + 1 });
+      setActiveOption((activeOption) => activeOption + 1);
     }
 
     // Esc key resets the view
     if (event.keyCode === 27) {
-      this.resetView();
+      resetView();
     }
   };
 
   // Change the currently selected option to the option the user moves their mouse over
-  onMouseOver = (event) => {
+  const onMouseOver = (event) => {
     const parent = event.target.parentNode;
     const optionIndex = [].indexOf.call(parent.children, event.target);
-    this.setState({ activeOption: optionIndex });
+    setActiveOption(optionIndex);
   };
 
   // Lists the autocomplete options provided in JSX format for rendering
-  listOptions = (optionName, index) => {
+  const listOptions = (optionName, index) => {
     let className = "option";
-    if (index === this.state.activeOption) {
+    if (index === activeOption) {
       className = "option-active";
     }
     return (
       <li
         className={className}
         key={optionName}
-        onClick={this.onClick}
-        onMouseOver={this.onMouseOver}
+        onClick={onClick}
+        onMouseOver={onMouseOver}
       >
         {optionName}
       </li>
     );
   };
 
-  render() {
-    const {
-      onChange,
-      onKeyDown,
-      onSubmit,
-      listOptions,
-      state: { filteredOptions, showOptions, userInput },
-    } = this;
-
-    // Provide the JSX code for the autocomplete suggestions list
-    let optionList;
-    if (showOptions && userInput) {
-      // JSX if there are matching pokemon names in the list
-      if (filteredOptions.length) {
-        // If there are more than 5 options, only return the first 5
-        if (filteredOptions.length > 5) {
-          optionList = (
-            <>
-              <div className="options-container-divider"></div>
-              <div className="options-container">
-                <ul className="options">
-                  {filteredOptions.slice(0, 5).map(listOptions)}
-                </ul>
-              </div>
-            </>
-          );
-        }
-
-        // If there are 5 or fewer options, return all
-        else {
-          optionList = (
-            <>
-              <div className="options-container-divider"></div>
-              <div className="options-container">
-                <ul className="options">{filteredOptions.map(listOptions)}</ul>
-              </div>
-            </>
-          );
-        }
+  // Provide the JSX code for the autocomplete suggestions list
+  let optionList;
+  if (showOptions && userInput) {
+    // JSX if there are matching pokemon names in the list
+    if (filteredOptions.length) {
+      // If there are more than 5 options, only return the first 5
+      if (filteredOptions.length > 5) {
+        optionList = (
+          <>
+            <div className="options-container-divider"></div>
+            <div className="options-container">
+              <ul className="options">
+                {filteredOptions.slice(0, 5).map(listOptions)}
+              </ul>
+            </div>
+          </>
+        );
       }
 
-      // JSX if there are no matching pokemon names in the list
+      // If there are 5 or fewer options, return all
       else {
         optionList = (
           <>
             <div className="options-container-divider"></div>
             <div className="options-container">
-              <p className="no-results">
-                <em>No pokémon found with that name!</em>
-              </p>
+              <ul className="options">{filteredOptions.map(listOptions)}</ul>
             </div>
           </>
         );
       }
     }
 
-    // Render the search button
-    const getSearchBtn = (options) => {
-      // If there are options available, render the enabled search button
-      if (options > 0) {
-        return (
-          <button
-            type="submit"
-            name="submit"
-            value=""
-            className="search-btn"
-            onClick={onSubmit}
-          >
-            <FontAwesomeIcon icon={faSearch} id="submitBtnIcon" />
-          </button>
-        );
-      }
-      // Otherwise the search button should be disabled to prevent searches for pokemon that do not exist
-      else {
-        return (
-          <button
-            type="submit"
-            id="submit"
-            name="submit"
-            value=""
-            disabled="disabled"
-            className="search-btn"
-            onClick={onSubmit}
-          >
-            <FontAwesomeIcon icon={faSearch} id="submitBtnIcon" />
-          </button>
-        );
-      }
-    };
-
-    return (
-      <div className={`search-container`}>
-        <div className={`search-bar`}>
-          <input
-            type="search"
-            name="search"
-            placeholder="Search pokémon"
-            className="search-box"
-            onChange={onChange}
-            onKeyDown={onKeyDown}
-            value={userInput}
-          />
-          <span className="search-divider"></span>
-          {getSearchBtn(filteredOptions.length)}
-        </div>
-        {optionList}
-      </div>
-    );
+    // JSX if there are no matching pokemon names in the list
+    else {
+      optionList = (
+        <>
+          <div className="options-container-divider"></div>
+          <div className="options-container">
+            <p className="no-results">
+              <em>No pokémon found with that name!</em>
+            </p>
+          </div>
+        </>
+      );
+    }
   }
-}
+
+  // Render the search button
+  const getSearchBtn = (options) => {
+    // If there are options available, render the enabled search button
+    if (options > 0) {
+      return (
+        <button
+          type="submit"
+          name="submit"
+          value=""
+          className="search-btn"
+          onClick={onSubmit}
+        >
+          <FontAwesomeIcon icon={faSearch} id="submitBtnIcon" />
+        </button>
+      );
+    }
+    // Otherwise the search button should be disabled to prevent searches for pokemon that do not exist
+    else {
+      return (
+        <button
+          type="submit"
+          id="submit"
+          name="submit"
+          value=""
+          disabled="disabled"
+          className="search-btn"
+          onClick={onSubmit}
+        >
+          <FontAwesomeIcon icon={faSearch} id="submitBtnIcon" />
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div className={`search-container`}>
+      <div className={`search-bar`}>
+        <input
+          type="search"
+          name="search"
+          placeholder="Search pokémon"
+          className="search-box"
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={userInput}
+        />
+        <span className="search-divider"></span>
+        {getSearchBtn(filteredOptions.length)}
+      </div>
+      {optionList}
+    </div>
+  );
+};
 
 export default SearchBar;
